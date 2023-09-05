e67a52e57651ae57fc263769525a93c160e428b5
Kevin Bui <kbui@moneymappress.com>
2023-09-05 12:59:27 -0400

    Added psycopg2 to connect to postgresql

--- app/app/test.py
+++ app/app/test.py
@@ -6,17 +6,18 @@ from django.test import SimpleTestCase
 
 from app import calc
 
+
 class CalcTests(SimpleTestCase):
     """ TEst the calc module"""
 
     def test_add_number(self):
         """Test adding numbers togeter"""
-        res = calc.add(5,6)
+        res = calc.add(5, 6)
 
         self.assertEqual(res, 11)
 
     def test_subtract_number(self):
         """Test subtract numbers together"""
-        res = calc.subtract(10,15)
+        res = calc.subtract(10, 15)
 
-        self.assert_(res, 5);
+        self.assert_(res, 5)
